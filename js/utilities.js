07ad9eb344bad39ecded58a95f7772b19fa52e77
jahidhasan6676 <jh18186676@gmail.com>
2024-09-23 12:34:29 +0600

    added the donation page dynamic

---
+++ js/utilities.js
@@ -0,0 +1,10 @@
+// input Value function--->
+function getInputFieldValueById(id){
+    const inputValue = parseFloat(document.getElementById(id).value);
+    return inputValue;
+}
+// text Value function--->
+function getTextValueById(id){
+    const textValue = parseFloat(document.getElementById(id).innerText);
+    return textValue;
+}
