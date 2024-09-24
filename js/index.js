cc0a987abf35fa49f40ba08d090120a821944d79
jahidhasan6676 <jh18186676@gmail.com>
2024-09-24 09:49:36 +0600

    added the sticky navbar and dom dynamic

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
-// donation for noakhali--->
+                        // donation for noakhali--->
 
 document.getElementById('donate-btn-noakhali').addEventListener('click', function(){
+
     const inputDonation = getInputFieldValueById('input-donation-noakhali');
-    
     const donateBalance = getTextValueById('donation-amount-noakhali');
     const myBalance = getTextValueById('my-balance');
 
@@ -14,10 +14,9 @@ document.getElementById('donate-btn-noakhali').addEventListener('click', functio
         my_modal_1.showModal();
     };
     
+    // donation add and minus
     const donationCollect = donateBalance + inputDonation;
     const myCurrentBalance = myBalance - inputDonation;
-    
-   
 
     document.getElementById('donation-amount-noakhali').innerText = donationCollect.toFixed(2);
     document.getElementById('my-balance').innerText = myCurrentBalance.toFixed(2);
@@ -32,16 +31,13 @@ document.getElementById('donate-btn-noakhali').addEventListener('click', functio
  
      const historyContainer = document.getElementById('history-container');
      historyContainer.insertBefore(historyDocuments, historyContainer.firstChild);
- 
-     
-
 })
 
-// donation for feni--->
+                       // donation for feni--->
 
 document.getElementById('donate-btn-feni').addEventListener('click', function(){
+
     const inputDonation = getInputFieldValueById('input-donation-feni');
-    
     const donateBalance = getTextValueById('donation-amount-feni');
     const myBalance = getTextValueById('my-balance');
 
@@ -52,10 +48,9 @@ document.getElementById('donate-btn-feni').addEventListener('click', function(){
     else{
         my_modal_2.showModal();
     };
-
+    // donation add and minus
     const donationCollect = donateBalance + inputDonation;
     const myCurrentBalance = myBalance - inputDonation;
-    
 
     document.getElementById('donation-amount-feni').innerText = donationCollect.toFixed(2);
     document.getElementById('my-balance').innerText = myCurrentBalance.toFixed(2);
@@ -72,11 +67,11 @@ document.getElementById('donate-btn-feni').addEventListener('click', function(){
  
 })
 
-// quota injured people donation--->
+                       // quota injured people donation--->
 
 document.getElementById('donate-btn-quota').addEventListener('click', function(){
+
     const inputDonation = getInputFieldValueById('input-donation-quota');
-    
     const donateBalance = getTextValueById('donation-amount-quota');
     const myBalance = getTextValueById('my-balance');
 
@@ -88,9 +83,9 @@ document.getElementById('donate-btn-quota').addEventListener('click', function()
         my_modal_3.showModal();
     };
 
+    // donation add and minus
     const donationCollect = donateBalance + inputDonation;
     const myCurrentBalance = myBalance - inputDonation;
-    
 
     document.getElementById('donation-amount-quota').innerText = donationCollect.toFixed(2);
     document.getElementById('my-balance').innerText = myCurrentBalance.toFixed(2);
@@ -98,6 +93,7 @@ document.getElementById('donate-btn-quota').addEventListener('click', function()
     // history added
     const historyDocuments = document.createElement('div');
     historyDocuments.className = 'w-full border-2 border-gray-100 p-3 rounded-lg mt-5';
+
     historyDocuments.innerHTML = `<p class=" font-medium primary-color mb-2">${inputDonation.toFixed(2)} Taka is Donated for Aid for Injured in the Quota Movement, Bangladesh</p>
     <p class="gray-color">Date: ${new Date().toLocaleString()}(Bangladesh Standard Times)`;
 
@@ -105,8 +101,10 @@ document.getElementById('donate-btn-quota').addEventListener('click', function()
     historyContainer.insertBefore(historyDocuments, historyContainer.firstChild);
 })
 
-// history tab--->
+                      // history tab--->
+
 document.getElementById('history-tab').addEventListener('click', function(){
+
     const historyTab = document.getElementById('history-tab');
     const donationTab = document.getElementById('donation-tab');
     
@@ -121,9 +119,10 @@ document.getElementById('history-tab').addEventListener('click', function(){
     
 })
 
-// donation tab--->
+                        // donation tab--->
 
 document.getElementById('donation-tab').addEventListener('click', function(){
+
     const historyTab = document.getElementById('history-tab');
     const donationTab = document.getElementById('donation-tab');
     
@@ -139,10 +138,10 @@ document.getElementById('donation-tab').addEventListener('click', function(){
     
 })
 
-// blog clickable--->
+                        // blog clickable--->
 
-document.getElementById('blog.btn').addEventListener('click', function(){
-    window.location.href = "./blog.html"
+document.getElementById('blog-btn').addEventListener('click', function(){
+    window.location.href = "./blog.html";
 })
 
 
